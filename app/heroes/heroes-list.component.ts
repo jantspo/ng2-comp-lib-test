8cf0acedf62c7ca9eb3e08c9fb10493b49acab52
John Sposato <jantspo@gmail.com>
2016-08-23 14:19:35 -0700

    messing around with tour of heroes tutorial

---
+++ app/heroes/heroes-list.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { Hero } from './hero';
+
+const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
+
+@Component({
+    template: `
+        <h2>My Heroes</h2>
+        <ul class="heroes">
+          <li *ngFor="let hero of heroes"
+              (click)="onSelect(hero)"
+              [class.selected]="hero === selectedHero">
+            <span class="badge">{{ hero.id}}</span> {{ hero.name}}
+          </li>
+        </ul>
+    `
+})
+export class HeroListComponent{
+    heroes = HEROES;
+    selectedHero: Hero;
+
+    onSelect(hero: Hero): void{
+        this.selectedHero = hero;
+    }
+}
